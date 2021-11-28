6d27b91991b4006a9a40232ea6b0fef7c31fad8f
throuz <64568129+throuz@users.noreply.github.com>
2021-11-28 23:53:23 +0800

    Complete fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,64 @@
 import "./App.css";
+// import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Advertise from "./components/Advertise";
+import data from "./data.json";
 
 function App() {
+  // const [error, setError] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  // const [items, setItems] = useState([]);
+  // const [images, setImages] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("https://fakestoreapi.com/products")
+  //     .then((res) => res.json())
+  //     .then(
+  //       (result) => {
+  //         setIsLoaded(true);
+  //         setItems(result);
+
+  //         const images = [];
+  //         for (let i = 0; i < result.length; i++) {
+  //           if (i > 9) {
+  //             break;
+  //           }
+  //           images.push(result[i].image);
+  //         }
+  //         setImages(images);
+  //       },
+  //       (error) => {
+  //         setIsLoaded(true);
+  //         setError(error);
+  //       }
+  //     );
+  // }, []);
+
+  // if (error) {
+  //   return <div>Error: {error.message}</div>;
+  // } else if (!isLoaded) {
+  //   return <div>Loading...</div>;
+  // } else {
+  //   return (
+  //     <div className="App">
+  //       <Header></Header>
+  //       <Advertise images={images}></Advertise>
+  //     </div>
+  //   );
+  // }
+
+  const images = [];
+  for (let i = 0; i < data.length; i++) {
+    if (i > 9) {
+      break;
+    }
+    images.push(data[i].image);
+  }
+
   return (
     <div className="App">
       <Header></Header>
-      <Advertise></Advertise>
+      <Advertise images={images}></Advertise>
     </div>
   );
 }
