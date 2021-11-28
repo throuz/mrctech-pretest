23414ccdb94fd12190e1306ed38e1957bfe6e649
throuz <64568129+throuz@users.noreply.github.com>
2021-11-29 07:03:15 +0800

    Complete all function

--- src/App.jsx
+++ src/App.jsx
@@ -6,65 +6,44 @@ import Countdown from "./components/Countdown";
 import ProductList from "./components/ProductList";
 import Categories from "./components/Categories";
 import GoTop from "./components/GoTop";
-import data from "./data.json";
 
 function App() {
-  // const [error, setError] = useState(null);
-  // const [isLoaded, setIsLoaded] = useState(false);
-  // const [products, setProducts] = useState([]);
-  // const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [productsData, setProductsData] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [images, setImages] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("https://fakestoreapi.com/products")
-  //     .then((res) => res.json())
-  //     .then(
-  //       (result) => {
-  //         setIsLoaded(true);
-  //         setProducts(result);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setProductsData(result);
+          setProducts(result);
 
-  //         const images = [];
-  //         for (let i = 0; i < result.length; i++) {
-  //           if (i > 9) {
-  //             break;
-  //           }
-  //           images.push(result[i].image);
-  //         }
-  //         setImages(images);
-  //       },
-  //       (error) => {
-  //         setIsLoaded(true);
-  //         setError(error);
-  //       }
-  //     );
-  // }, []);
-
-  // if (error) {
-  //   return <div>Error: {error.message}</div>;
-  // } else if (!isLoaded) {
-  //   return <div>Loading...</div>;
-  // } else {
-  //   return (
-  //     <div className="App">
-  //       <Header></Header>
-  //       <Advertise images={images}></Advertise>
-  //     </div>
-  //   );
-  // }
-  const [products, setProducts] = useState(data);
-
-  const images = [];
-  for (let i = 0; i < data.length; i++) {
-    if (i > 9) {
-      break;
-    }
-    images.push(data[i].image);
-  }
+          const images = [];
+          for (let i = 0; i < result.length; i++) {
+            if (i > 9) {
+              break;
+            }
+            images.push(result[i].image);
+          }
+          setImages(images);
+          setIsLoaded(true);
+        },
+        (error) => {
+          setError(error);
+          setIsLoaded(true);
+        }
+      );
+  }, []);
 
   const SearchFilter = (e) => {
     const searchValue = e.target.value.toLowerCase();
 
     setProducts(
-      data.filter(
+      productsData.filter(
         (product) =>
           product.title.toLowerCase().includes(searchValue) ||
           product.description.toLowerCase().includes(searchValue)
@@ -74,27 +53,35 @@ function App() {
 
   const categoryFilter = (category) => {
     if (category === "All") {
-      setProducts(data);
+      setProducts(productsData);
     } else {
       setProducts(
-        data.filter((product) => product.category === category.toLowerCase())
+        productsData.filter(
+          (product) => product.category === category.toLowerCase()
+        )
       );
     }
   };
 
-  return (
-    <div className="App">
-      <Header
-        SearchFilter={SearchFilter}
-        categoryFilter={categoryFilter}
-      ></Header>
-      <Advertise images={images}></Advertise>
-      <Countdown></Countdown>
-      <ProductList products={products}></ProductList>
-      <Categories categoryFilter={categoryFilter}></Categories>
-      <GoTop></GoTop>
-    </div>
-  );
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="App">
+        <Header
+          SearchFilter={SearchFilter}
+          categoryFilter={categoryFilter}
+        ></Header>
+        <Advertise images={images}></Advertise>
+        <Countdown></Countdown>
+        <ProductList products={products}></ProductList>
+        <Categories categoryFilter={categoryFilter}></Categories>
+        <GoTop></GoTop>
+      </div>
+    );
+  }
 }
 
 export default App;
